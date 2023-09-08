aa008008c4b3cdcf427024ef07d5731ea98b59e6
PARKSEONGA2218065 <144421592+PARKSEONGA2218065@users.noreply.github.com>
2023-09-08 16:44:28 +0900

    2023.09.08 quiz
    2주차 과제 연산자 입니다
    a = A, b= B 이고 이름은 성아로 정했습니다.

---
+++ test.py
@@ -0,0 +1,12 @@
+number = input("숫자를 입력하세요")
+print(number)
+name = input("이름을 입력하세요.")
+age = input("나이를 입력하세요")
+print(name, age)
+
+a = input("숫자를 입력하세요")
+b = input("숫자를 입력하세요")
+성아 = input("숫자를 입력하세요")
+
+a = input("A")
+b = input("")
